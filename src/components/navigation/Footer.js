ac992b519fc7282d5fa6ad0cd14efe01e398f522
Stefan <Stefischer@mail.com>
2021-03-27 15:56:40 -0400

    Updated links in Footer

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -23,10 +23,6 @@ class Footer extends Component {
                         </h6>
                         <hr className="pink darken-1 accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px" }} />
                     </Link>
-                    <p>
-                    This is some sample flavor text that will be changed before production. It is meant
-                    to display some text underneath the company name.
-                    </p>
                 </Col>
                 <Col md={2} lg={2} xl={2} className="mb-4">
                     <h6 className="text-uppercase font-weight-bold">
@@ -58,16 +54,10 @@ class Footer extends Component {
                     </h6>
                     <hr className="pink darken-1 accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px" }} />
                     <p>
-                    <a href="#!">Get a Quote</a>
+                    <Link to="/gallery">Portfolio</Link>
                     </p>
                     <p>
-                    <a href="#!">Portfolio</a>
-                    </p>
-                    <p>
-                    <a href="#!">About</a>
-                    </p>
-                    <p>
-                    <a href="#!">Help</a>
+                    <Link to="/about">About</Link>
                     </p>
                 </Col>
                 <Col md={4} lg={3} xl={3} className="mb-4">
@@ -76,16 +66,10 @@ class Footer extends Component {
                     </h6>
                     <hr className="pink darken-1 accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px" }} />
                     <p>
-                    <i className="fa fa-home mr-3" /> New York, NY 10012, US
-                    </p>
-                    <p>
                     <i className="fa fa-envelope mr-3" /> sophiaaksoy@gmail.com
                     </p>
                     <p>
-                    <i className="fa fa-phone mr-3" /> + 01 234 567 88
-                    </p>
-                    <p>
-                    <i className="fa fa-print mr-3" /> + 01 234 567 89
+                    <i className="fa fa-phone mr-3" /> 516-768-6363
                     </p>
                 </Col>
                 </Row>
@@ -94,28 +78,18 @@ class Footer extends Component {
             <div className="text-center">
                 <ul className="list-unstyled list-inline">
                 <li className="list-inline-item">
-                    <a className="btn-floating btn-sm btn-fb mx-1 sandre-color" target="_blank" rel="noopener noreferrer" href={SocialMedia.facebook}>
+                    <a className="btn-floating btn-sm btn-fb mx-1 sandre-color" target="_blank" rel="noopener noreferrer" href={SocialMedia.facebook.url}>
                     <i className="fab fa-facebook-f" />
                     </a>
                 </li>
                 <li className="list-inline-item">
-                    <a className="btn-floating btn-sm btn-tw mx-1 sandre-color" target="_blank" rel="noopener noreferrer" href={SocialMedia.facebook}>
-                    <i className="fab fa-twitter" />
-                    </a>
-                </li>
-                <li className="list-inline-item">
-                    <a className="btn-floating btn-sm btn-gplus mx-1 sandre-color" target="_blank" rel="noopener noreferrer" href={SocialMedia.facebook}>
+                    <a className="btn-floating btn-sm btn-gplus mx-1 sandre-color" target="_blank" rel="noopener noreferrer" href={SocialMedia.instagram.url}>
                     <i className="fab fa-instagram" />
                     </a>
                 </li>
                 <li className="list-inline-item">
-                    <a className="btn-floating btn-sm btn-li mx-1 sandre-color" target="_blank" rel="noopener noreferrer" href={SocialMedia.facebook}>
+                    <a className="btn-floating btn-sm btn-li mx-1 sandre-color" target="_blank" rel="noopener noreferrer" href={SocialMedia.linkedin.url}>
                     <i className="fab fa-linkedin-in" />
-                    </a>
-                </li>
-                <li className="list-inline-item">
-                    <a className="btn-floating btn-sm btn-dribbble mx-1 sandre-color" target="_blank" rel="noopener noreferrer" href={SocialMedia.facebook}>
-                    <i className="fab fa-pintrest" />
                     </a>
                 </li>
                 </ul>
